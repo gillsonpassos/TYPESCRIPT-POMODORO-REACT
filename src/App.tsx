5356b8742b3796da7e4fe6c6d43dc12d5d0a8e80
gillsonpassos <oliveirapassos07@outlook.com>
2022-11-22 16:15:30 -0300

    UseInterval(React-Hook)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { PomodoroTimer } from './components/pomodoro-timer';
 
 function App(): JSX.Element {
-  return <div className="App">Olá MUndo!</div>;
+  return (
+    <div className="App">
+      <PomodoroTimer defaultPomodoroTime={1500} />
+    </div>
+  );
 }
 
 export default App;
